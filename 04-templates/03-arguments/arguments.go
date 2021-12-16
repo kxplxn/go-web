e7eefd11928316b9a918f7dbc2441819f8790e5f
Alican Demirtas <alcadeta@icloud.com>
2021-12-16 20:59:36 +0000

    030403 Go Templates: Arguments

---
+++ 04-templates/03-arguments/arguments.go
@@ -0,0 +1,23 @@
+package _030403_arguments_
+
+import (
+	"fmt"
+)
+
+func Demo() {
+	fmt.Println("\n030403 Go Templates: Arguments")
+
+	//args := os.Args
+	//port := os.Args[1]
+	//message := os.Args[2]
+	//
+	//fmt.Println(args)
+
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	w.Write([]byte(message))
+	//})
+	//
+	//http.ListenAndServe(":"+port, nil)
+
+	fmt.Println("Uncomment lines 10-20 in `arguments.go` to demo this chapter.")
+}
--- 04-templates/templates.go
+++ 04-templates/templates.go
@@ -3,9 +3,11 @@ package _0304_templates_
 import (
 	parsingAndExecuting "go-web/04-templates/01-parsingAndExecuting"
 	actions "go-web/04-templates/02-actions"
+	arguments "go-web/04-templates/03-arguments"
 )
 
 func Demos() {
 	parsingAndExecuting.Demo()
 	actions.Demo()
+	arguments.Demo()
 }
