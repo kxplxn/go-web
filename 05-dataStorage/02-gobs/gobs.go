838e4403b14b95b5f88d59c335bf69abcc6cac54
Alican Demirtas <alcadeta@icloud.com>
2021-12-17 21:20:07 +0000

    030502 Data Storage: Working With Gobs

---
+++ 05-dataStorage/02-gobs/gobs.go
@@ -0,0 +1,41 @@
+package _040502_gobPackage_
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
+func writeBinaryFile(data interface{}, file string) {
+	buf := bytes.Buffer{}
+	encoder := gob.NewEncoder(&buf)
+	encoder.Encode(data)
+	ioutil.WriteFile(file, buf.Bytes(), 0600)
+}
+
+func readBinaryFile(data interface{}, file string) {
+	raw, _ := ioutil.ReadFile(file)
+	buf := bytes.NewBuffer(raw)
+	decoder := gob.NewDecoder(buf)
+	decoder.Decode(data)
+}
+
+func Demo() {
+	fmt.Println("\n030502 Data Storage: Working With Gobs")
+
+	file := "05-dataStorage/02-gobs/person"
+
+	person := Person{"Bob", "Barker"}
+	writeBinaryFile(person, file)
+
+	readPerson := Person{}
+	readBinaryFile(&readPerson, file)
+
+	fmt.Printf("%+v\n", readPerson)
+}
--- 05-dataStorage/dataStorage.go
+++ 05-dataStorage/dataStorage.go
@@ -2,8 +2,10 @@ package _0405_dataStorage_
 
 import (
 	structs "go-web/05-dataStorage/01-structs"
+	gobs "go-web/05-dataStorage/02-gobs"
 )
 
 func Demos() {
 	structs.Demo()
+	gobs.Demo()
 }
